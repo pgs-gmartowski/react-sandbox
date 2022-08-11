d411cfea84f8a2de2298f38be661282bf6e19019
Grzegorz <gmartowski@pgs-soft.com>
2022-08-11 15:57:34 +0200

    -1 react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import Container from './components/Container';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import Tile from './components/Tile';
+import {Button} from '@mui/material';
 
 const queryClient = new QueryClient();
 
 function App() {
+
+  const [state, setState] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Kliknięto ${state} razy`;
+  })
+
+  const getRandomized = () => {
+    console.log(Math.random());
+  }
+
+  useMemo(() => getRandomized(), [count]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
@@ -17,6 +32,8 @@ function App() {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
+          <Button onClick={() => setState(state + 1)}>Klik</Button>
+          <Button onClick={() => setCount(count + 1)}>Klik 1</Button>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -26,7 +43,7 @@ function App() {
             Learn React
           </a>
         </header>
-        <Container children={(data) => <Tile data={data} />} />
+        {/*<Container children={(data) => <Tile data={data} />} />*/}
       </div>
       <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
